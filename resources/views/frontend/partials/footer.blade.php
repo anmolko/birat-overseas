6ab7219f80ee64c7726876f3be43011b42e64323
anmolko <anmol@canosoft.com.np>
2023-04-22 13:53:46 +0545

    Initial commit

---
+++ resources/views/frontend/partials/footer.blade.php
@@ -0,0 +1,222 @@
+<!--Site Footer Start-->
+<footer class="site-footer">
+    <div class="site-footer-bg" style="background-image: url(assets/images/backgrounds/site-footer-bg.png);">
+    </div>
+    <div class="container">
+        <div class="site-footer__top">
+            <div class="row">
+                <div class="col-xl-3 col-lg-6 col-md-6 wow fadeInUp" data-wow-delay="100ms">
+                    <div class="footer-widget__column footer-widget__about">
+                        <div class="footer-widget__logo">
+                            <a href="index.html"><img src="assets/images/resources/footer-logo.png" alt=""></a>
+                        </div>
+                        <div class="footer-widget__about-text-box">
+                            <p class="footer-widget__about-text">Aliqua id fugiat nostrud irure ex duis ea quis
+                                id quis ad et. Sunt qui esse pariatur duis deserunt.</p>
+                        </div>
+                        <div class="site-footer__social">
+                            <a href="#"><i class="fab fa-twitter"></i></a>
+                            <a href="#"><i class="fab fa-facebook"></i></a>
+                            <a href="#"><i class="fab fa-pinterest-p"></i></a>
+                            <a href="#"><i class="fab fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-6 col-md-6 wow fadeInUp" data-wow-delay="200ms">
+                    <div class="footer-widget__column footer-widget__contact clearfix">
+                        <h3 class="footer-widget__title">Contact</h3>
+                        <ul class="footer-widget__contact-list list-unstyled clearfix">
+                            <li>
+                                <div class="icon">
+                                    <span class="icon-email"></span>
+                                </div>
+                                <div class="text">
+                                    <p><a href="mailto:needhelp@company.com">needhelp@company.com</a></p>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="icon">
+                                    <span class="icon-pin"></span>
+                                </div>
+                                <div class="text">
+                                    <p>30 Commercial Road Fratton, Australia</p>
+                                </div>
+                            </li>
+                        </ul>
+                        <div class="footer-widget__open-hour">
+                            <h3 class="footer-widget__open-hour-title">Open Hours</h3>
+                            <h3 class="footer-widget__open-hour-text">Mon – Sat: 8:00 am to 6:00 pm Sunday:
+                                Closed</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-6 col-md-6 wow fadeInUp" data-wow-delay="300ms">
+                    <div class="footer-widget__column footer-widget__gallery clearfix">
+                        <h3 class="footer-widget__title">Instagram</h3>
+                        <ul class="footer-widget__gallery-list list-unstyled clearfix">
+                            <li>
+                                <div class="footer-widget__gallery-img">
+                                    <img src="assets/images/resources/footer-widget-gallery-img-1.jpg" alt="">
+                                    <a href="#"><span class="fa fa-link"></span></a>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="footer-widget__gallery-img">
+                                    <img src="assets/images/resources/footer-widget-gallery-img-2.jpg" alt="">
+                                    <a href="#"><span class="fa fa-link"></span></a>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="footer-widget__gallery-img">
+                                    <img src="assets/images/resources/footer-widget-gallery-img-3.jpg" alt="">
+                                    <a href="#"><span class="fa fa-link"></span></a>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="footer-widget__gallery-img">
+                                    <img src="assets/images/resources/footer-widget-gallery-img-4.jpg" alt="">
+                                    <a href="#"><span class="fa fa-link"></span></a>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="footer-widget__gallery-img">
+                                    <img src="assets/images/resources/footer-widget-gallery-img-5.jpg" alt="">
+                                    <a href="#"><span class="fa fa-link"></span></a>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="footer-widget__gallery-img">
+                                    <img src="assets/images/resources/footer-widget-gallery-img-6.jpg" alt="">
+                                    <a href="#"><span class="fa fa-link"></span></a>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-6 col-md-6 wow fadeInUp" data-wow-delay="400ms">
+                    <div class="footer-widget__column footer-widget__newsletter">
+                        <h3 class="footer-widget__title">Newsletter</h3>
+                        <p class="footer-widget__newsletter-text">Subscribe our newsletter to get our <br>
+                            latest update & news.</p>
+                        <form class="footer-widget__newsletter-form">
+                            <div class="footer-widget__newsletter-input-box">
+                                <input type="email" placeholder="Email address" name="email">
+                                <button type="submit" class="footer-widget__newsletter-btn"><i
+                                        class="far fa-paper-plane"></i></button>
+                            </div>
+                        </form>
+                        <div class="footer-widget__phone">
+                            <div class="footer-widget__phone-icon">
+                                <span class="icon-telephone"></span>
+                            </div>
+                            <div class="footer-widget__phone-text">
+                                <a href="tel:9200368090">+92 (003) 68-090</a>
+                                <p>Call to Our Experts</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="site-footer__bottom">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="site-footer__bottom-inner">
+                        <p class="site-footer__bottom-text">© All Copyright 2023 by <a href="#">Insur.com</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!--Site Footer End-->
+
+
+</div><!-- /.page-wrapper -->
+
+
+<div class="mobile-nav__wrapper">
+    <div class="mobile-nav__overlay mobile-nav__toggler"></div>
+    <!-- /.mobile-nav__overlay -->
+    <div class="mobile-nav__content">
+        <span class="mobile-nav__close mobile-nav__toggler"><i class="fa fa-times"></i></span>
+
+        <div class="logo-box">
+            <a href="index.html" aria-label="logo image"><img src="assets/images/resources/logo-2.png" width="143"
+                                                              alt="" /></a>
+        </div>
+        <!-- /.logo-box -->
+        <div class="mobile-nav__container"></div>
+        <!-- /.mobile-nav__container -->
+
+        <ul class="mobile-nav__contact list-unstyled">
+            <li>
+                <i class="fa fa-envelope"></i>
+                <a href="mailto:needhelp@packageName__.com">needhelp@insur.com</a>
+            </li>
+            <li>
+                <i class="fa fa-phone-alt"></i>
+                <a href="tel:666-888-0000">666 888 0000</a>
+            </li>
+        </ul><!-- /.mobile-nav__contact -->
+        <div class="mobile-nav__top">
+            <div class="mobile-nav__social">
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-facebook-square"></a>
+                <a href="#" class="fab fa-pinterest-p"></a>
+                <a href="#" class="fab fa-instagram"></a>
+            </div><!-- /.mobile-nav__social -->
+        </div><!-- /.mobile-nav__top -->
+
+
+
+    </div>
+    <!-- /.mobile-nav__content -->
+</div>
+<!-- /.mobile-nav__wrapper -->
+
+<div class="search-popup">
+    <div class="search-popup__overlay search-toggler"></div>
+    <!-- /.search-popup__overlay -->
+    <div class="search-popup__content">
+        <form action="#">
+            <label for="search" class="sr-only">search here</label><!-- /.sr-only -->
+            <input type="text" id="search" placeholder="Search Here..." />
+            <button type="submit" aria-label="search submit" class="thm-btn">
+                <i class="icon-magnifying-glass"></i>
+            </button>
+        </form>
+    </div>
+</div>
+<a href="#" data-target="html" class="scroll-to-target scroll-to-top"><i class="fa fa-angle-up"></i></a>
+
+
+<script src="{{ asset('assets/frontend/vendors/jquery/jquery-3.6.0.min.js') }}"></script>
+<script src="{{ asset('assets/frontend/vendors/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('assets/frontend/vendors/jarallax/jarallax.min.js') }}"></script>
+<script src="{{ asset('assets/frontend/vendors/jquery-ajaxchimp/jquery.ajaxchimp.min.js') }}"></script>
+<script src="{{ asset('assets/frontend/vendors/jquery-appear/jquery.appear.min.js') }}"></script>
+<script src="{{ asset('assets/frontend/vendors/jquery-circle-progress/jquery.circle-progress.min.js') }}"></script>
+<script src="{{ asset('assets/frontend/vendors/jquery-magnific-popup/jquery.magnific-popup.min.js') }}"></script>
+<script src="{{ asset('assets/frontend/vendors/jquery-validate/jquery.validate.min.js') }}"></script>
+<script src="{{ asset('assets/frontend/vendors/nouislider/nouislider.min.js') }}"></script>
+<script src="{{ asset('assets/frontend/vendors/odometer/odometer.min.js') }}"></script>
+<script src="{{ asset('assets/frontend/vendors/swiper/swiper.min.js') }}"></script>
+<script src="{{ asset('assets/frontend/vendors/tiny-slider/tiny-slider.min.js') }}"></script>
+<script src="{{ asset('assets/frontend/vendors/wnumb/wNumb.min.js') }}"></script>
+<script src="{{ asset('assets/frontend/vendors/wow/wow.js') }}"></script>
+<script src="{{ asset('assets/frontend/vendors/isotope/isotope.js') }}"></script>
+<script src="{{ asset('assets/frontend/vendors/countdown/countdown.min.js') }}"></script>
+<script src="{{ asset('assets/frontend/vendors/owl-carousel/owl.carousel.min.js') }}"></script>
+<script src="{{ asset('assets/frontend/vendors/bxslider/jquery.bxslider.min.js') }}"></script>
+<script src="{{ asset('assets/frontend/vendors/bootstrap-select/js/bootstrap-select.min.js') }}"></script>
+<script src="{{ asset('assets/frontend/vendors/vegas/vegas.min.js') }}"></script>
+<script src="{{ asset('assets/frontend/vendors/jquery-ui/jquery-ui.js') }}"></script>
+<script src="{{ asset('assets/frontend/vendors/timepicker/timePicker.js') }}"></script>
+<script src="{{ asset('assets/frontend/vendors/circleType/jquery.circleType.js') }}"></script>
+<script src="{{ asset('assets/frontend/vendors/circleType/jquery.lettering.min.js') }}"></script>
+<script src="{{asset('assets/frontend/js/birat.js')}}"></script>
+
+</body>
+</html>
---
+++ resources/views/frontend/partials/header.blade.php
@@ -0,0 +1,220 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <!-- favicons Icons -->
+    {{--    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('assets/frontend/images/favicons/favicon-32x32.png')}}" />--}}
+    {{--    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('assets/frontend/images/favicons/favicon-16x16.png')}}" />--}}
+
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <meta name="author" content=" BIRAT OVERSEAS PVT. LTD - leading manpower company">
+    <meta name="description"
+          content="@if(!empty(@$setting_data->meta_description)) {{ucwords(@$setting_data->meta_description)}} @else BIRAT OVERSEAS PVT. LTD - leading manpower company @endif"/>
+    <meta name="keywords"
+          content="@if(!empty(@$setting_data->meta_tags)) {{@$setting_data->meta_tags}} @else BIRAT OVERSEAS PVT. LTD - leading manpower company @endif">
+    <meta name="csrf-token" content="{{ csrf_token() }}"/>
+    <link rel="canonical" href="https://biratoverseas.com/"/>
+
+    @if (Request::is('/'))
+        <title>@if(!empty(@$setting_data->website_name)) {{ucwords(@$setting_data->website_name)}} @else  BIRAT OVERSEAS
+            PVT. LTD - leading manpower company @endif </title>
+    @else
+        <title>@yield('title')
+            | @if(!empty(@$setting_data->website_name)) {{ucwords(@$setting_data->website_name)}} @else  BIRAT OVERSEAS
+            PVT. LTD - leading manpower company @endif </title>
+    @endif
+
+    <meta property="og:title" content="@if(!empty(@$setting_data->meta_title)) {{ucwords(@$setting_data->meta_title)}} @else   BIRAT OVERSEAS PVT. LTD - leading manpower company @endif" />
+    <meta property="og:type" content="Consultancy" />
+    <meta property="og:url" content="https://biratoverseas.com/" />
+    <meta property="og:site_name" content=" BIRAT OVERSEAS PVT. LTD - leading manpower company" />
+    <meta property="og:description" content="@if(!empty(@$setting_data->meta_description)) {{ucwords(@$setting_data->meta_description)}} @else  BIRAT OVERSEAS PVT. LTD - leading manpower company @endif " />
+
+    <link rel="shortcut icon" type="image/x-icon" href="{{ (@$setting_data->favicon) ? asset('/images/settings/'.@$setting_data->favicon):asset('assets/backend/images/canosoft-favicon.png') }}">
+
+
+
+    <!-- fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com/">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+
+    <link
+        href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&amp;display=swap"
+        rel="stylesheet">
+
+
+    <link rel="stylesheet" href="{{ asset('assets/frontend/vendors/bootstrap/css/bootstrap.min.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('assets/frontend/vendors/animate/animate.min.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('assets/frontend/vendors/animate/custom-animate.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('assets/frontend/vendors/fontawesome/css/all.min.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('assets/frontend/vendors/jarallax/jarallax.css') }}"/>
+    <link rel="stylesheet"
+          href="{{ asset('assets/frontend/vendors/jquery-magnific-popup/jquery.magnific-popup.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('assets/frontend/vendors/nouislider/nouislider.min.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('assets/frontend/vendors/nouislider/nouislider.pips.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('assets/frontend/vendors/odometer/odometer.min.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('assets/frontend/vendors/swiper/swiper.min.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('assets/frontend/vendors/insur-icons/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/frontend/vendors/insur-two-icon/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/frontend/vendors/tiny-slider/tiny-slider.min.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('assets/frontend/vendors/reey-font/stylesheet.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('assets/frontend/vendors/owl-carousel/owl.carousel.min.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('assets/frontend/vendors/owl-carousel/owl.theme.default.min.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('assets/frontend/vendors/bxslider/jquery.bxslider.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('assets/frontend/vendors/bootstrap-select/css/bootstrap-select.min.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('assets/frontend/vendors/vegas/vegas.min.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('assets/frontend/vendors/jquery-ui/jquery-ui.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('assets/frontend/vendors/timepicker/timePicker.css') }}"/>
+
+    <!-- template styles -->
+    <link rel="stylesheet" id="langLtr" href="{{ asset('assets/frontend/css/birat.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('assets/frontend/css/birat-responsive.css') }}"/>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id={{@$setting_data->google_analytics}}"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', '{{@$setting_data->google_analytics}}');
+    </script>
+    @yield('css')
+    @stack('styles')
+
+</head>
+
+<body class="custom-cursor">
+
+<div class="custom-cursor__cursor"></div>
+<div class="custom-cursor__cursor-two"></div>
+
+<div class="preloader">
+    <div class="preloader__image"></div>
+</div>
+<!-- /.preloader -->
+
+<div class="page-wrapper">
+    <header class="main-header-three clearfix">
+        <div class="main-header__top">
+            <div class="container">
+                <div class="main-header__top-inner">
+                    <div class="main-header__top-address">
+                        <ul class="list-unstyled main-header__top-address-list">
+                            <li>
+                                <i class="icon">
+                                    <span class="icon-pin"></span>
+                                </i>
+                                <div class="text">
+                                    <p>{{@$setting_data->address ?? ''}}</p>
+                                </div>
+                            </li>
+                            <li>
+                                <i class="icon">
+                                    <span class="icon-email"></span>
+                                </i>
+                                <div class="text">
+                                    <p><a href="mailto:{{@$setting_data->email ?? ''}}">{{@$setting_data->email ?? ''}}</a></p>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="main-header__top-right">
+                        <div class="main-header__top-menu-box">
+                            <ul class="list-unstyled main-header__top-menu">
+                                <li><a href="#">Make a claim</a></li>
+                                <li><a href="#"> FAQs</a></li>
+                                <li><a href="#">About</a></li>
+                            </ul>
+                        </div>
+                        <div class="main-header__top-social-box">
+                            <div class="main-header__top-social">
+                                @if(@$setting_data->facebook)
+                                    <a href="#"><i class="fab fa-facebook"></i></a>
+                                @endif
+                                @if(@$setting_data->instagram)
+                                    <a href="#"><i class="fab fa-instagram"></i></a>
+                                @endif
+                                @if(@$setting_data->youtube)
+                                    <a href="#"><i class="fab fa-youtube"></i></a>
+                                @endif
+                                @if(@$setting_data->linkedin)
+                                    <a href="#"><i class="fab fa-linkedin"></i></a>
+                                @endif
+                                @if(!empty(@$setting_data->ticktock))
+                                    <a href="#"><i class="fab fa-tiktok"></i></a>
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <nav class="main-menu main-menu-three clearfix">
+            <div class="main-menu-three__wrapper clearfix">
+                <div class="container">
+                    <div class="main-menu-three__wrapper-inner clearfix">
+                        <div class="main-menu-three__left">
+                            <div class="main-menu-three__logo">
+                                <a href="/"><img class="home-logo" src="{{asset('/images/settings/'.@$setting_data->logo)}}" alt=""></a>
+                            </div>
+                            <div class="main-menu-three__main-menu-box">
+                                <a href="#" class="mobile-nav__toggler"><i class="fa fa-bars"></i></a>
+                                <ul class="main-menu__list">
+                                    <li
+                                        class="current megamenu megamenu-clickable megamenu-clickable--toggler">
+                                        <a href="/">Home</a>
+                                    </li>
+                                    @if(!empty($top_nav_data))
+                                        @foreach($top_nav_data as $nav)
+                                            @if(!empty($nav->children[0]))
+                                                <li class="dropdown">
+                                                    <a href="#">{{ @$nav->name ?? @$nav->title }}</a>
+                                                    <ul>
+                                                        @foreach($nav->children[0] as $childNav)
+                                                        <li>
+                                                            <a href="{{get_menu_url($childNav->type, $childNav)}}" target="{{@$childNav->target ? '_blank':''}}">
+                                                                {{ @$childNav->name ?? @$childNav->title ??''}}</a>
+                                                        </li>
+                                                        @endforeach
+                                                    </ul>
+                                                </li>
+                                            @else
+                                                <li>
+                                                    <a href="{{get_menu_url($nav->type, $nav)}}" target="{{@$nav->target ? '_blank':''}}">
+                                                        {{ @$nav->name ?? @$nav->title ??''}}
+                                                    </a>
+                                                </li>
+                                            @endif
+                                        @endforeach
+                                    @else
+                                        <li>
+                                            <a href="{{get_menu_url($nav->type, $nav)}}" target="{{@$nav->target ? '_blank':''}}">
+                                                {{ @$nav->name ?? @$nav->title ??''}}
+                                            </a>
+                                        </li>
+                                    @endif
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="main-menu-three__right">
+                            <div class="main-menu-three__search-get-quote-btn">
+                                <div class="main-menu-three__search-box">
+                                    <a href="#"
+                                       class="main-menu-three__search search-toggler icon-magnifying-glass"></a>
+                                </div>
+                                <div class="main-menu-three__get-quote-btn-box">
+                                    <a href="{{route('contact')}}" class="thm-btn main-menu-three__get-quote-btn">Get in
+                                        Touch</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <div class="stricky-header stricked-menu main-menu main-menu-three">
+        <div class="sticky-header__content"></div><!-- /.sticky-header__content -->
+    </div>
